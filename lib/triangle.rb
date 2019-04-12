7f57fe8835e61bbdf7e674266598c5c8f3f7a8d0
Carden Z <ephemere@protonmail.com>
2019-04-12 00:14:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,55 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    valid_triangle?
+
+    if length1 == length2 && length2 == length3
+      :equilateral
+    elsif length1 == length2 || length3 == length2 || length1 == length3
+      :isosceles
+    elsif length1 != length2 && length3 != length2 && length1 != length3
+      :scalene
+    end
+  end
+
+  def valid_triangle?
+    greater_than_zero? && triangle_inequality? ? true : exception
+  end
+
+  def exception
+    binding.pry
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
+  end
+
+  def greater_than_zero?
+    lengths = [length1, length2, length3]
+    lengths.all? {|l| l > 0}
+  end
+
+  def triangle_inequality?
+    sum1 = length1 + length2
+    sum2 = length2 + length3
+    sum3 = length3 + length1
+
+    sum1 > length3 && sum2 > length1 && sum3 > length2
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle"
+    end
+  end
+
 end
