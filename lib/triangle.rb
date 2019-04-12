3a80326274d2d8a04a7ef31d70b306468ec1fbce
Carden Z <ephemere@protonmail.com>
2019-04-12 00:24:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,15 @@ class Triangle
   end
 
   def kind
-    valid_triangle?
+    if valid_triangle?
 
-    if length1 == length2 && length2 == length3
-      :equilateral
-    elsif length1 == length2 || length3 == length2 || length1 == length3
-      :isosceles
-    elsif length1 != length2 && length3 != length2 && length1 != length3
-      :scalene
+      if length1 == length2 && length2 == length3
+        :equilateral
+      elsif length1 == length2 || length3 == length2 || length1 == length3
+        :isosceles
+      elsif length1 != length2 && length3 != length2 && length1 != length3
+        :scalene
+      end
     end
   end
 
